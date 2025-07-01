79647b3d43e8cccd9d0e848216afde14533087f2
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-07-01 10:17:51 -0300

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path"
 
 //here we have the route and all the local imports
 import notesRoutes from "./routes/notesRoutes.js"
@@ -13,11 +14,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
-//the line below is a middleware that we add 
-app.use(cors({
-    origin: "http://localhost:5173",
-})); //This allows every request from every single URL
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+} //This allows every request from every single URL
+
+
 app.use(express.json()); // This middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
@@ -28,6 +33,13 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+});
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
